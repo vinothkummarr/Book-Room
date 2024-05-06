63696d275b04f3847fd48fbe54d68995151f6eb2
vinothkumarr <vinothkumarr8987@gmail.com>
2024-05-06 10:56:31 +0530

    Deployment settings done

--- app.js
+++ app.js
@@ -5,14 +5,31 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://myAtlasDBUser:12345@myatlasclusteredu.o9c75np.mongodb.net/local_library?retryWrites=true&w=majority&appName=myAtlasClusterEDU";
-
+const dev_db_url = "mongodb+srv://myAtlasDBUser:12345@myatlasclusteredu.o9c75np.mongodb.net/local_library?retryWrites=true&w=majority&appName=myAtlasClusterEDU";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 const catalogRouter = require("./routes/catalog"); 
+const compression = require("compression");
+const helmet = require("helmet");
 var app = express();
 
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
+
+app.use(
+  helmet.contentSecurityPolicy({
+    directives: {
+      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
+    },
+  }),
+);
 main().catch((err) => console.log(err));
 async function main() {
   await mongoose.connect(mongoDB);
@@ -25,6 +42,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
