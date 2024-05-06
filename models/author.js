58f55c99bd1c75ce7eaca4b5442b602a188874a7
vinothkumarr <vinothkumarr8987@gmail.com>
2024-05-06 10:26:18 +0530

    Completed

---
+++ models/author.js
@@ -0,0 +1,56 @@
+const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
+
+const Schema = mongoose.Schema;
+
+const AuthorSchema = new Schema({
+  first_name: { type: String, required: true, maxLength: 100 },
+  family_name: { type: String, required: true, maxLength: 100 },
+  date_of_birth: { type: Date },
+  date_of_death: { type: Date },
+});
+
+// Virtual for author's full name
+AuthorSchema.virtual("name").get(function () {
+  // to avoid errors in cases where an author does not have either a family name or first name
+  // make sure we handle the exception by returning an empty string in that case
+
+  let fullname = "";
+  if (this.first_name && this.family_name) {
+    fullname = `${this.family_name}, ${this.first_name}`;
+  }
+  return fullname;
+});
+
+// Virtual for author's URL
+AuthorSchema.virtual("url").get(function () {
+  // not using arrow func because the this object is needed
+  return `/catalog/author/${this._id}`;
+});
+
+// Virtual for author's birth and death dates
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth
+    ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death
+    ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
+    : "";
+});
+
+AuthorSchema.virtual("lifespan").get(function () {
+  return this.date_of_birth_formatted + " - " + this.date_of_death_formatted;
+});
+
+AuthorSchema.virtual("date_of_birth_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_birth).toISODate(); // format 'YYYY-MM-DD'
+});
+
+AuthorSchema.virtual("date_of_death_yyyy_mm_dd").get(function () {
+  return DateTime.fromJSDate(this.date_of_death).toISODate(); // format 'YYYY-MM-DD'
+});
+
+module.exports = mongoose.model("Author", AuthorSchema);
